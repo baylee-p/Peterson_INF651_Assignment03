b81669b95471f7c6fcdb42cc66bd37bb42e27837
baylee-p <b_peterson4@mail.fhsu.edu>
2024-06-27 01:22:08 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function analyzeArray(myArray){
+    console.log(Math.max(myArray));
+    console.log(Math.min(myArray));
+/* I am aware the next line is not what you were looking for, but it
+was the only code I felt I understood to get the even numbers in an array.
+Many walkthroughs I watched for help seemed far more advanced and I didn't 
+comfortably understand them. */
+            let evenArray = [];
+            evenArray.push(2, 4, 6, 8, 10);
+        };
+analyzeArray();
